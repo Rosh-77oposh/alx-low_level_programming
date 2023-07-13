3e6510a677efa34e4c8060ef2b4d7472c2adbd07
Rosh-77oposh <opondoronny03@gmail.com>
2023-07-13 13:22:09 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("sixe of a char: %ls byte(s)\n", sizeof(char));
+	printf("sixe of a int: %ls byte(s)\n", sizeof(int));
+	printf("sixe of a long int: %ls byte(s)\n", sizeof(long int));
+	printf("sixe of a long long int: %ls byte(s)\n", sizeof(long long int));
+	printf("sixe of a float: %ls byte(s)\n", sizeof(float));
+	return (0);
+}
