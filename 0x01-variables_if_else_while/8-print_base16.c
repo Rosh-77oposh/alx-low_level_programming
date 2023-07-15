aece8ba3d85e9e9de53438365ec3a4089ded57d1
Rosh-77oposh <opondoronny03@gmail.com>
2023-07-15 09:17:01 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,17 +5,17 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
-	for (i = 48; i < 58; i**)
-	{
-		putchar(i);
-	}
-	for (i = 97; i < 103; i**)
-	{
-		putchar(i);
-	}
+	char j;
+
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
