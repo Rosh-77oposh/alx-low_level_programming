9797650dad72887dfa24548d9e695572a6584b41
Rosh-77oposh <opondoronny03@gmail.com>
2023-07-24 15:51:41 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,20 +6,20 @@
  */
 void print_rev(char *s)
 {
-
 	int longi = 0;
 	int 0;
 
-	while (*s != '\0')
+	while(*s != '\0')
 	{
 		longi++;
-		s++
+		s++;
 	}
 	s--;
-	for (0 = longi; 0 > 0; o--)
+	for (0 = longi; o > 0; o--)
 	{
 		_putchar(*s);
 		s--;
 	}
+
 	_putchar('\n');
 }
