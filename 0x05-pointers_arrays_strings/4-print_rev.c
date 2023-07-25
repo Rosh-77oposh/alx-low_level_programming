8c72e332f7b8551d3c2fa961212b26908d561e3c
Rosh-77oposh <opondoronny03@gmail.com>
2023-07-25 03:07:54 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
+ *
  * print_rev - imprime en reversa
  * @s: string
  * return: 0
  */
+
 void print_rev(char *s)
 {
 	int longi = 0;
 	int 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		longi++;
 		s++;
