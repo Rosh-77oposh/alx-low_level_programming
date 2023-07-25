f3c8bf26cd52527d386bc97145d4c31c45624ef0
Rosh-77oposh <opondoronny03@gmail.com>
2023-07-25 03:32:33 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts2 - function should print only ne character out of two starting with the first one
+ * @str: input
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int 0;
+
+	while (*y != '\0')
+
+	{
+		y++;
+		longi++;
+	}
+	t = longi++;
+	for (o = 0 ; o <= t ; o++)
+
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
+}
