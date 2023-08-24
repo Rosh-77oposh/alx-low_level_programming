afb4b20637650bbd5889a20c8f0c266b360db1b3
Rosh-77oposh <opondoronny03@gmail.com>
2023-08-24 08:35:36 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct list_s - singly linked list
+ * @sr: string - (malloc string)
+ * @len: strng length
+ * @next: points to next node
+ *
+ * Description: singly linked list node structure for holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list;
+
+size_t print_list(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+size_t list_len(const list_t *h);
+void free_list(list_t *head);
+
+#endif
